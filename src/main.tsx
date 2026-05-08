effb9edbdd867b2ff5e264e2ac2e797bcb1827bd
bra83 <55473935+bra83@users.noreply.github.com>
2026-05-08 04:26:50 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ try {
   // Register Service Worker for PWA
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').catch(err => {
+      navigator.serviceWorker.register('sw.js').catch(err => {
         console.log('ServiceWorker registration failed: ', err);
       });
     });
